1094c9663adfa3c47ab395b6d649eed32bbb6008
BuildTools <unconfigured@null.spigotmc.org>
2021-07-22 18:51:36 -0600

    initial commit

---
+++ src/main/java/dansplugins/wildpets/WildPets.java
@@ -0,0 +1,17 @@
+package dansplugins.wildpets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WildPets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
