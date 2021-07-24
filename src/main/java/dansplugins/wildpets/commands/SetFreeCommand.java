8b422bd0522ab9804de2b5f844e5279ac4bc3049
BuildTools <unconfigured@null.spigotmc.org>
2021-07-24 01:55:09 -0600

    Implemented the Set Free Command

---
+++ src/main/java/dansplugins/wildpets/commands/SetFreeCommand.java
@@ -0,0 +1,35 @@
+package dansplugins.wildpets.commands;
+
+import dansplugins.wildpets.data.EphemeralData;
+import dansplugins.wildpets.data.PersistentData;
+import dansplugins.wildpets.objects.Pet;
+import dansplugins.wildpets.objects.PetList;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetFreeCommand {
+
+    public boolean execute(CommandSender sender) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        Pet pet = EphemeralData.getInstance().getPetSelectionForPlayer(player);
+
+        if (pet == null) {
+            player.sendMessage(ChatColor.RED + "No pet selected.");
+            return false;
+        }
+
+        String petName = pet.getName();
+
+        PetList petList = PersistentData.getInstance().getPetList(player);
+        petList.removePet(pet);
+        player.sendMessage(ChatColor.GREEN + petName + " has been set free.");
+        return true;
+    }
+
+}
