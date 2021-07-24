a6113d928717e790d48be6332a44a9d4a8c9adba
BuildTools <unconfigured@null.spigotmc.org>
2021-07-23 18:34:33 -0600

    Added the version field to the main class and changed the version to v0.3

--- src/main/java/dansplugins/wildpets/WildPets.java
+++ src/main/java/dansplugins/wildpets/WildPets.java
@@ -12,6 +12,8 @@ public final class WildPets extends JavaPlugin {
         return instance;
     }
 
+    private final String version = "v0.3";
+
     @Override
     public void onEnable() {
         instance = this;
@@ -27,5 +29,9 @@ public final class WildPets extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         CommandInterpreter commandInterpreter = new CommandInterpreter();
         return commandInterpreter.interpretCommand(sender, label, args);
+    }
+
+    public String getVersion() {
+        return version;
     }
 }
