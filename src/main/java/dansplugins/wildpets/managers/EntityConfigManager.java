bc3657bbf9e82465949cc7bb12e2ae7955972926
BuildTools <unconfigured@null.spigotmc.org>
2021-07-25 15:14:28 -0600

    Changed chance to succeed for all current entity configurations to 50%

--- src/main/java/dansplugins/wildpets/managers/EntityConfigManager.java
+++ src/main/java/dansplugins/wildpets/managers/EntityConfigManager.java
@@ -39,35 +39,35 @@ public class EntityConfigManager {
     private void initialize() {
 
         // passive mobs
-        entityConfigs.add(new EntityConfig("Axilotl", 0.10, Material.KELP, 20, true));
-        entityConfigs.add(new EntityConfig("Bat", 0.25, Material.PUMPKIN_PIE, 1, true));
-        entityConfigs.add(new EntityConfig("Cat", 0.25, Material.SALMON, 5, true));
-        entityConfigs.add(new EntityConfig("Chicken", 0.30, Material.WHEAT_SEEDS, 10, true));
-        entityConfigs.add(new EntityConfig("Cod", 0.10, Material.KELP, 20, true));
-        entityConfigs.add(new EntityConfig("Cow", 0.25, Material.WHEAT, 30, true));
-        entityConfigs.add(new EntityConfig("Donkey", 0.15, Material.CARROT, 5, true));
-        entityConfigs.add(new EntityConfig("Fox", 0.25, Material.SWEET_BERRIES, 10, true));
-        entityConfigs.add(new EntityConfig("Glow_Squid", 0.10, Material.KELP, 20, true));
-        entityConfigs.add(new EntityConfig("Horse", 0.15, Material.APPLE, 5, true));
-        entityConfigs.add(new EntityConfig("Mooshroom", 0.25, Material.RED_MUSHROOM, 10, true));
-        entityConfigs.add(new EntityConfig("Mule", 0.15, Material.APPLE, 5, true));
-        entityConfigs.add(new EntityConfig("Ocelot", 0.25, Material.COD, 10, true));
-        entityConfigs.add(new EntityConfig("Parrot", 0.30, Material.PUMPKIN_SEEDS, 10, true));
-        entityConfigs.add(new EntityConfig("Pig", 0.25, Material.CARROT, 15, true));
-        entityConfigs.add(new EntityConfig("Piglin_Baby", 0.10, Material.NETHER_WART, 5, true));
-        entityConfigs.add(new EntityConfig("Polar_Bear_Baby", 0.10, Material.SALMON, 15, true));
-        entityConfigs.add(new EntityConfig("Pufferfish", 0.10, Material.KELP, 20, true));
-        entityConfigs.add(new EntityConfig("Rabbit", 0.25, Material.DANDELION, 5, true));
-        entityConfigs.add(new EntityConfig("Salmon", 0.10, Material.KELP, 20, true));
-        entityConfigs.add(new EntityConfig("Sheep", 0.25, Material.WHEAT, 10, true));
-        entityConfigs.add(new EntityConfig("Skeleton_Horse", 0.10, Material.BONE, 10, true));
-        entityConfigs.add(new EntityConfig("Snow_Golem", 0.05, Material.SNOWBALL, 30, true));
-        entityConfigs.add(new EntityConfig("Squid", 0.10, Material.KELP, 20, true));
-        entityConfigs.add(new EntityConfig("Strider", 0.05, Material.NETHER_WART, 30, true));
-        entityConfigs.add(new EntityConfig("Tropical_Fish", 0.10, Material.KELP, 20, true));
-        entityConfigs.add(new EntityConfig("Turtle", 0.25, Material.SEAGRASS, 15, true));
-        entityConfigs.add(new EntityConfig("Villager", 0.25, Material.POTATO, 10, false));
-        entityConfigs.add(new EntityConfig("Wandering Trader", 0.25, Material.GOLD_INGOT, 10, false));
+        entityConfigs.add(new EntityConfig("Axilotl", 0.5, Material.KELP, 20, true));
+        entityConfigs.add(new EntityConfig("Bat", 0.5, Material.PUMPKIN_PIE, 1, true));
+        entityConfigs.add(new EntityConfig("Cat", 0.5, Material.SALMON, 5, true));
+        entityConfigs.add(new EntityConfig("Chicken", 0.5, Material.WHEAT_SEEDS, 10, true));
+        entityConfigs.add(new EntityConfig("Cod", 0.5, Material.KELP, 20, true));
+        entityConfigs.add(new EntityConfig("Cow", 0.5, Material.WHEAT, 30, true));
+        entityConfigs.add(new EntityConfig("Donkey", 0.5, Material.CARROT, 5, true));
+        entityConfigs.add(new EntityConfig("Fox", 0.5, Material.SWEET_BERRIES, 10, true));
+        entityConfigs.add(new EntityConfig("Glow_Squid", 0.5, Material.KELP, 20, true));
+        entityConfigs.add(new EntityConfig("Horse", 0.5, Material.APPLE, 5, true));
+        entityConfigs.add(new EntityConfig("Mooshroom", 0.5, Material.RED_MUSHROOM, 10, true));
+        entityConfigs.add(new EntityConfig("Mule", 0.5, Material.APPLE, 5, true));
+        entityConfigs.add(new EntityConfig("Ocelot", 0.5, Material.COD, 10, true));
+        entityConfigs.add(new EntityConfig("Parrot", 0.5, Material.PUMPKIN_SEEDS, 10, true));
+        entityConfigs.add(new EntityConfig("Pig", 0.5, Material.CARROT, 15, true));
+        entityConfigs.add(new EntityConfig("Piglin_Baby", 0.5, Material.NETHER_WART, 5, true));
+        entityConfigs.add(new EntityConfig("Polar_Bear_Baby", 0.5, Material.SALMON, 15, true));
+        entityConfigs.add(new EntityConfig("Pufferfish", 0.5, Material.KELP, 20, true));
+        entityConfigs.add(new EntityConfig("Rabbit", 0.5, Material.DANDELION, 5, true));
+        entityConfigs.add(new EntityConfig("Salmon", 0.5, Material.KELP, 20, true));
+        entityConfigs.add(new EntityConfig("Sheep", 0.5, Material.WHEAT, 10, true));
+        entityConfigs.add(new EntityConfig("Skeleton_Horse", 0.5, Material.BONE, 10, true));
+        entityConfigs.add(new EntityConfig("Snow_Golem", 0.5, Material.SNOWBALL, 30, true));
+        entityConfigs.add(new EntityConfig("Squid", 0.5, Material.KELP, 20, true));
+        entityConfigs.add(new EntityConfig("Strider", 0.5, Material.NETHER_WART, 30, true));
+        entityConfigs.add(new EntityConfig("Tropical_Fish", 0.5, Material.KELP, 20, true));
+        entityConfigs.add(new EntityConfig("Turtle", 0.5, Material.SEAGRASS, 15, true));
+        entityConfigs.add(new EntityConfig("Villager", 0.5, Material.POTATO, 10, false));
+        entityConfigs.add(new EntityConfig("Wandering Trader", 0.5, Material.GOLD_INGOT, 10, false));
 
         // neutral mobs
         // TODO
