6dfba9168f95d38fe8b29e09da4e71650aa99e5e
BuildTools <unconfigured@null.spigotmc.org>
2021-07-25 05:42:28 -0600

    Altered success chances for passive entities.

--- src/main/java/dansplugins/wildpets/managers/EntityConfigManager.java
+++ src/main/java/dansplugins/wildpets/managers/EntityConfigManager.java
@@ -40,32 +40,32 @@ public class EntityConfigManager {
 
         // passive mobs
         entityConfigs.add(new EntityConfig("Axilotl", 0.10, Material.KELP, 20));
-        entityConfigs.add(new EntityConfig("Bat", 0.10, Material.PUMPKIN_PIE, 1));
-        entityConfigs.add(new EntityConfig("Cat", 0.10, Material.SALMON, 5));
-        entityConfigs.add(new EntityConfig("Chicken", 0.10, Material.WHEAT_SEEDS, 10));
+        entityConfigs.add(new EntityConfig("Bat", 0.25, Material.PUMPKIN_PIE, 1));
+        entityConfigs.add(new EntityConfig("Cat", 0.25, Material.SALMON, 5));
+        entityConfigs.add(new EntityConfig("Chicken", 0.30, Material.WHEAT_SEEDS, 10));
         entityConfigs.add(new EntityConfig("Cod", 0.10, Material.KELP, 20));
-        entityConfigs.add(new EntityConfig("Cow", 0.10, Material.WHEAT, 30));
-        entityConfigs.add(new EntityConfig("Donkey", 0.10, Material.CARROT, 5));
-        entityConfigs.add(new EntityConfig("Fox", 0.10, Material.SWEET_BERRIES, 10));
+        entityConfigs.add(new EntityConfig("Cow", 0.25, Material.WHEAT, 30));
+        entityConfigs.add(new EntityConfig("Donkey", 0.15, Material.CARROT, 5));
+        entityConfigs.add(new EntityConfig("Fox", 0.25, Material.SWEET_BERRIES, 10));
         entityConfigs.add(new EntityConfig("Glow_Squid", 0.10, Material.KELP, 20));
-        entityConfigs.add(new EntityConfig("Horse", 0.10, Material.APPLE, 5));
-        entityConfigs.add(new EntityConfig("Mooshroom", 0.10, Material.RED_MUSHROOM, 10));
-        entityConfigs.add(new EntityConfig("Mule", 0.10, Material.APPLE, 5));
-        entityConfigs.add(new EntityConfig("Ocelot", 0.10, Material.COD, 10));
-        entityConfigs.add(new EntityConfig("Parrot", 0.10, Material.PUMPKIN_SEEDS, 10));
-        entityConfigs.add(new EntityConfig("Pig", 0.10, Material.CARROT, 15));
+        entityConfigs.add(new EntityConfig("Horse", 0.15, Material.APPLE, 5));
+        entityConfigs.add(new EntityConfig("Mooshroom", 0.25, Material.RED_MUSHROOM, 10));
+        entityConfigs.add(new EntityConfig("Mule", 0.15, Material.APPLE, 5));
+        entityConfigs.add(new EntityConfig("Ocelot", 0.25, Material.COD, 10));
+        entityConfigs.add(new EntityConfig("Parrot", 0.30, Material.PUMPKIN_SEEDS, 10));
+        entityConfigs.add(new EntityConfig("Pig", 0.25, Material.CARROT, 15));
         entityConfigs.add(new EntityConfig("Piglin_Baby", 0.10, Material.NETHER_WART, 5));
         entityConfigs.add(new EntityConfig("Polar_Bear_Baby", 0.10, Material.SALMON, 15));
         entityConfigs.add(new EntityConfig("Pufferfish", 0.10, Material.KELP, 20));
-        entityConfigs.add(new EntityConfig("Rabbit", 0.10, Material.DANDELION, 5));
+        entityConfigs.add(new EntityConfig("Rabbit", 0.25, Material.DANDELION, 5));
         entityConfigs.add(new EntityConfig("Salmon", 0.10, Material.KELP, 20));
-        entityConfigs.add(new EntityConfig("Sheep", 0.10, Material.WHEAT, 10));
+        entityConfigs.add(new EntityConfig("Sheep", 0.25, Material.WHEAT, 10));
         entityConfigs.add(new EntityConfig("Skeleton_Horse", 0.10, Material.BONE, 10));
-        entityConfigs.add(new EntityConfig("Snow_Golem", 0.10, Material.SNOWBALL, 30));
+        entityConfigs.add(new EntityConfig("Snow_Golem", 0.05, Material.SNOWBALL, 30));
         entityConfigs.add(new EntityConfig("Squid", 0.10, Material.KELP, 20));
-        entityConfigs.add(new EntityConfig("Strider", 0.10, Material.NETHER_WART, 30));
+        entityConfigs.add(new EntityConfig("Strider", 0.05, Material.NETHER_WART, 30));
         entityConfigs.add(new EntityConfig("Tropical_Fish", 0.10, Material.KELP, 20));
-        entityConfigs.add(new EntityConfig("Turtle", 0.10, Material.SEAGRASS, 15));
+        entityConfigs.add(new EntityConfig("Turtle", 0.25, Material.SEAGRASS, 15));
     }
 
     private EntityConfig getDefaultConfiguration() {
