487d9dcc53344f2ab81a03481e4ea34cbb8a8906
BuildTools <unconfigured@null.spigotmc.org>
2021-07-29 19:20:27 -0600

    Added skeleton event handler methods to DamageEffectsAndDeathHandler.java

---
+++ src/main/java/dansplugins/wildpets/eventhandlers/DamageEffectsAndDeathHandler.java
@@ -0,0 +1,34 @@
+package dansplugins.wildpets.eventhandlers;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.*;
+
+public class DamageEffectsAndDeathHandler implements Listener {
+
+    @EventHandler()
+    public void handle(EntityDamageByEntityEvent event) {
+        // TODO
+    }
+
+    @EventHandler()
+    public void handle(AreaEffectCloudApplyEvent event) {
+        // TODO
+    }
+
+    @EventHandler()
+    public void handle(LingeringPotionSplashEvent event) {
+        // TODO
+    }
+
+    @EventHandler()
+    public void handle(EntityDeathEvent event) {
+        // TODO
+    }
+
+    @EventHandler()
+    public void handle(PotionSplashEvent event) {
+        // TODO
+    }
+
+}
