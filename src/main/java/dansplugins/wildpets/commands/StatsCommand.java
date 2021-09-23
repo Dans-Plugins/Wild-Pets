063e0f1342db2f1789162ec2f028b9075f22e1e8
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-23 14:51:13 -0600

    Altered some colors in StatsCommand.java

--- src/main/java/dansplugins/wildpets/commands/StatsCommand.java
+++ src/main/java/dansplugins/wildpets/commands/StatsCommand.java
@@ -9,8 +9,8 @@ public class StatsCommand {
     // TODO: add statistics object with persistent data like number of times players have attempted to tame
 
     public boolean execute(CommandSender sender) {
-        sender.sendMessage(ChatColor.GREEN + "=== Wild Pets Statistics ===");
-        sender.sendMessage("Number of Pets: " + PersistentData.getInstance().getAllPets().size());
+        sender.sendMessage(ChatColor.AQUA + "=== Wild Pets Statistics ===");
+        sender.sendMessage(ChatColor.AQUA + "Number of Pets: " + PersistentData.getInstance().getAllPets().size());
         return true;
     }
 
