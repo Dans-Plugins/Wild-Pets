f946bed2e62e18c8b8d10ff0114e02c1ef6f9c95
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-12 19:46:32 -0700

    Added a skeleton class for the revoke access command.

---
+++ src/main/java/dansplugins/wildpets/commands/RevokeAccessCommand.java
@@ -0,0 +1,11 @@
+package dansplugins.wildpets.commands;
+
+import org.bukkit.command.CommandSender;
+
+public class RevokeAccessCommand {
+
+    public void execute(CommandSender sender) {
+        // TODO: implement
+    }
+
+}
