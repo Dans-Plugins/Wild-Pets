cf9bc6f68177a9b96f7b9a4a9bf4172fdc1d5cdd
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-18 23:37:25 -0700

    Altered BreedEventHandler.java

--- src/main/java/dansplugins/wildpets/eventhandlers/BreedEventHandler.java
+++ src/main/java/dansplugins/wildpets/eventhandlers/BreedEventHandler.java
@@ -24,31 +24,31 @@ public class BreedEventHandler implements Listener {
         Entity parent1 = event.getFather();
         Entity parent2 = event.getMother();
 
-        PersistentData.getInstance().addNewPet(player, child);
-
-        Pet newPet = PersistentData.getInstance().getPet(child);
-
-        player.sendMessage(ChatColor.AQUA + "You have a new pet! Type /wp select " + newPet.getName() + " to select it.");
-
         Pet petParent1 = PersistentData.getInstance().getPet(parent1);
         Pet petParent2 = PersistentData.getInstance().getPet(parent2);
 
-        if (petParent1 != null) {
-            newPet.addParent(petParent1.getAssignedID());
-            petParent1.addChild(newPet.getAssignedID());
-        }
-        else {
-            newPet.addParent(-1);
-        }
+        PersistentData.getInstance().addNewPet(player, child);
 
-        if (petParent2 != null) {
-            newPet.addParent(petParent2.getAssignedID());
-            petParent2.addChild(newPet.getAssignedID());
-        }
-        else {
-            newPet.addParent(-1);
-        }
+        if (petParent1 != null || petParent2 != null) {
+            Pet newPet = PersistentData.getInstance().getPet(child);
+            player.sendMessage(ChatColor.AQUA + "You have a new pet! Type /wp select " + newPet.getName() + " to select it.");
 
+            if (petParent1 != null) {
+                newPet.addParent(petParent1.getAssignedID());
+                petParent1.addChild(newPet.getAssignedID());
+            }
+            else {
+                newPet.addParent(-1);
+            }
+
+            if (petParent2 != null) {
+                newPet.addParent(petParent2.getAssignedID());
+                petParent2.addChild(newPet.getAssignedID());
+            }
+            else {
+                newPet.addParent(-1);
+            }
+        }
     }
 
 }
