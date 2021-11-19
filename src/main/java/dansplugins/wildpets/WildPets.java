86600b9daf36764831608956063ac661354bb78b
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-19 00:12:09 -0700

    Changed version to v1.3-alpha-2

--- src/main/java/dansplugins/wildpets/WildPets.java
+++ src/main/java/dansplugins/wildpets/WildPets.java
@@ -25,7 +25,7 @@ public final class WildPets extends AbstractPonderPlugin {
         return instance;
     }
 
-    private final String version = "v1.3-alpha-1";
+    private final String version = "v1.3-alpha-2";
 
     @Override
     public void onEnable() {
