44ed359d379b5e2a9dbc7659fa39a208f1246217
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-19 00:42:42 -0700

    Altered PetRecord.java

--- src/main/java/dansplugins/wildpets/objects/PetRecord.java
+++ src/main/java/dansplugins/wildpets/objects/PetRecord.java
@@ -3,15 +3,18 @@ package dansplugins.wildpets.objects;
 import preponderous.ponder.modifiers.Savable;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 public class PetRecord implements Savable {
     private UUID uniqueID;
+    String name;
     private UUID ownerUUID;
     private int assignedID;
 
     public PetRecord(Pet pet) {
         uniqueID = pet.getUniqueID();
+        name = pet.getName();
         ownerUUID = pet.getOwnerUUID();
         assignedID = pet.getAssignedID();
     }
@@ -49,5 +52,12 @@ public class PetRecord implements Savable {
     @Override
     public void load(Map<String, String> map) {
         // TODO: implement
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PetRecord petRecord = (PetRecord) o;
+        return uniqueID.equals(petRecord.uniqueID);
     }
 }
